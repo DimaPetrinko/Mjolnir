1655da5ac1b4453742c12073d6f06d54911d6431
Dmytrii Petrinko <petrinko6@gmail.com>
2019-11-08 00:17:58 +0200

    Added music config and three songs

---
+++ Assets/Scripts/Data/MusicConfig.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Data
+{
+	[CreateAssetMenu(fileName = "MusicConfig", menuName = "Configs/Music config")]
+	public sealed class MusicConfig : ScriptableObject
+	{
+		[SerializeField] private AudioClip[] songs;
+
+		public AudioClip GetRandomSong()
+		{
+			var randomIndex = Random.Range(0, songs.Length);
+			return songs[randomIndex];
+		}
+	}
+}
